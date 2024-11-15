082d5a7cc2f22701eabdffd93e016a47a09fac38
Akshath-Nagulapally <akshathnag04@gmail.com>
2024-11-15 17:33:57 -0600

    added simple logging of image's url and user message

--- src/server.js
+++ src/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const { MessagingResponse } = require('twilio').twiml;
+const axios = require('axios');
 
 const app = express();
-const port = 1337;
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
+  const mediaUrl = req.body.MediaUrl0; // Image URL (if any)
+  const mediaType = req.body.MediaContentType0; // Media type (e.g., image/jpeg)
+  const user_message = req.body.Body //get the user's message from the request body
+  const images = [];
 
-  //Future scope: Basically I need to be able to handle when a user inputs both an image and a screenshot
+  //basically: If the user inputs only text, handle it by saying "sorry, i only accept screenshots", once image is there then invoke the same function thing.
 
-  user_message = req.body.Body //get the user's message from the request body
-  console.log(user_message)
-  twiml.message(user_message);
+
 
   res.type('text/xml').send(twiml.toString());
 });
